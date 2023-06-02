1dc6896c2cf5759a5ec8fce4743679ba41a137b8
code0xff <ian.jungyong.um@gmail.com>
2023-06-02 11:28:06 +0900

    build: Edit build with npm

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import { ApiPromise, Keyring, util } from '@noir/api';
+import { ApiPromise, WsProvider, Keyring } from '@pinot/api';
+import { decodeAddress } from '@pinot/util';
 
 // Transafer
 (async () => {
-  const api = await ApiPromise.create();
+  const provider = new WsProvider('wss://dev.lenoir.io');
+  const api = await ApiPromise.create({ provider });
   const keyring = new Keyring({ type: 'sr25519' });
   const alice = keyring.addFromUri('//Alice');
-  const bob = util.decodeAddress('ugCQDn2mpP6u7q9_GCzmvVLIq0VusOde1szsIhy1qYVAUzA0')
+  const bob = decodeAddress('ugCQCNNaa_iu2h-wwd6ierrcc2APH8IZI1azc087F_P8-IAo')
 
   console.log((await api.query.system.account(alice.addressRaw)).toHuman());
   console.log((await api.query.system.account(bob)).toHuman());
 
-  console.log((await api.tx.balances.transfer(bob, 10000000000000n).signAndSend(alice)).toHuman());
+  // console.log((await api.tx.balances.transfer(bob, 1152920000000000000n).signAndSend(alice)).toHuman());
 })();
