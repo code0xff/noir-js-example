de9df003e82e7b0f22531ff77809c8eb8ba9c39b
code0xff <ian.jungyong.um@gmail.com>
2023-06-25 17:09:36 +0900

    feat: Change test data

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import { ApiPromise, WsProvider, Keyring } from '@pinot/api';
-import { decodeAddress } from '@pinot/util';
+import { ApiPromise, WsProvider, Keyring } from "@pinot/api";
+import { decodeAddress } from "@pinot/util";
 
 // Transafer
 (async () => {
-  const provider = new WsProvider('wss://dev.lenoir.io');
+  // const provider = new WsProvider("ws://dev.lenoir.io");
+  const provider = new WsProvider("ws://127.0.0.1:9944");
   const api = await ApiPromise.create({ provider });
-  const keyring = new Keyring({ type: 'sr25519' });
-  const alice = keyring.addFromUri('//Alice');
-  const bob = decodeAddress('ugCQCNNaa_iu2h-wwd6ierrcc2APH8IZI1azc087F_P8-IAo')
+  const keyring = new Keyring({ type: "sr25519" });
+  const alice = keyring.addFromUri("//Alice");
+  const bob = decodeAddress("u5wEDEw1cvmFa6zy5RnBBAtdLgVF1TILwuF4evZBDlQRUClM");
 
   console.log((await api.query.system.account(alice.addressRaw)).toHuman());
   console.log((await api.query.system.account(bob)).toHuman());
 
-  // console.log((await api.tx.balances.transfer(bob, 1152920000000000000n).signAndSend(alice)).toHuman());
+  // console.log(
+  //   (
+  //     await api.tx.balances.transfer(bob, 1000000000000000n).signAndSend(alice)
+  //   ).toHuman()
+  // );
 })();
